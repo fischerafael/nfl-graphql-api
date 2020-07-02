ad27718944c4a46d828e30ed1d58524540d54dbf
Rafael Fischer <rafaelsanfischer@gmail.com>
2020-07-02 15:38:21 -0300

    .gitgnore update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import startServer from './utils/startServer'
 import typeDefs from './graphql/typeDefs'
 import resolvers from './graphql/resolvers'
-require('dotenv').config()
 
 startServer({ typeDefs, resolvers })
